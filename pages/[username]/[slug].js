aa8fa3bde193756a6cf3023bee49a6b02bd364dc
yintii <keleheart1@gmail.com>
2023-02-26 21:33:04 -0800

    initial commit for the course

---
+++ pages/[username]/[slug].js
@@ -0,0 +1,7 @@
+export default function PostPage({ }) {
+  return (
+    <main>
+      <h1>Single User Post</h1>
+    </main>
+  )
+}
---
+++ pages/[username]/index.js
@@ -0,0 +1,7 @@
+export default function ProfilePage({ }) {
+  return (
+    <main>
+      <h2>User Posts</h2>
+    </main>
+  )
+}
